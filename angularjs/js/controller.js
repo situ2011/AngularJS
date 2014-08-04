4dac3017fcfb1a8a84bba8df3142c159cfa25497
situ2011 <gao_st@126.com>
2014-08-04 11:51:14 +0800

    临时提交

--- angularjs/js/controller.js
+++ angularjs/js/controller.js
@@ -1,12 +1,19 @@
-// module
-var umService = angular.module( 'UserManage', []);
+// 定义一个模块, 并引入依赖的angular模块
+var umService = angular.module( 'UserManage', [ 'ngRoute' ] );
 
 function umRouteConfig ( $routeProvider ) {
-	console.log( $routeProvider )
+	// console.log( $routeProvider );
 	$routeProvider
 	.when( '/', {
 		controller: ListController,
 		templateUrl: 'list.html'
+	})
+	.when( '/update/:id', {
+		controller: UpdateController,
+		templateUrl: 'detail.html'
+	})
+	.when( '/delete', {
+		
 	})
 	.otherwise({
       redirectTo: '/'
@@ -15,6 +22,26 @@ function umRouteConfig ( $routeProvider ) {
 
 umService.config( umRouteConfig );
 
-function ListController ( $scope ) {
-	$scope.message = 'message';
+function ListController ( $scope, $http ) {
+	$http.get( 'server/user.json' ).success( function ( data, status, headers, config ) {
+		console.log( data );
+		$scope.users = data;
+	});
+}
+
+function UpdateController ( $scope, $http, $routeParams ) {
+	var id = $routeParams.id;
+	console.log( id );
+	$http.get( 'server/user.json' ).success( function ( data, status, headers, config ) {
+		//console.log( data );
+		 $scope.user = data;
+		 console.log($scope.user)
+		var len=data.length;
+		for(var i=0;i<len;i++){
+			console.log(data[i].id);
+			if(id==data[i].id){
+				$scope.xiuUser=data[i];	
+			}			
+		}
+	});
 }
